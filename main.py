3e6b607dc156ab7269ded225d8934a2e0770a8e6
Nathaniel Chien <nlchien22@gmail.com>
2018-07-31 15:38:04 -0700

    merge

--- main.py
+++ main.py
@@ -91,6 +91,108 @@ class Display_Profile(webapp2.RequestHandler):
 
 
 
+import webapp2
+import jinja2
+import os
+import logging
+
+from google.appengine.ext import ndb
+from google.appengine.api import users
+
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class Profile(ndb.Model):
+    first_name = ndb.StringProperty()
+    last_name = ndb.StringProperty()
+    education = ndb.StringProperty()
+    experience = ndb.StringProperty()
+    industry = ndb.StringProperty()
+    email = ndb.StringProperty()
+    resume = ndb.BlobProperty()
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        logging.info('This is the main handler')
+        #This part of the code is to direc the user to either login, logout, or create account
+        login_url = users.create_login_url('/')
+        logout_url = users.create_logout_url('/')
+        create_account = users.create_login_url('/create')
+        current_person = ''
+        current_user = users.get_current_user()
+        if not current_user:
+            current_user = None
+            templateVars = {
+                'login_url': login_url,
+                'current_user': current_user,
+                'logout_url': logout_url,
+                'create_account': create_account,
+                'current_person': current_person,
+            }
+            template = env.get_template('templates/home.html')
+            self.response.write(template.render(templateVars))
+        else:
+            current_email = current_user.email()
+            #pinpoints the right account for the person who just logged in
+            if Profile.query().filter(Profile.email == current_email).get():
+                current_person = Profile.query().filter(Profile.email == current_email).get()
+                templateVars = {
+                    'login_url': login_url,
+                    'current_user': current_user,
+                    'logout_url': logout_url,
+                    'create_account': create_account,
+                    'current_person': current_person,
+                }
+                template = env.get_template('templates/home.html')
+                self.response.write(template.render(templateVars))
+            else:
+                print("hello")
+                self.redirect('/fail')
+
+
+
+class CreateProfile(webapp2.RequestHandler):
+    def get(self):
+        template = env.get_template('templates/create_profile.html')
+        self.response.write(template.render())
+
+    def post(self):
+        email = users.get_current_user().email()
+        first_name = self.request.get('first_name')
+        last_name = self.request.get('last_name')
+        education = self.request.get('education')
+        experience = self.request.get('experience')
+        industry = self.request.get('industry')
+        profile = Profile(email=email, first_name=first_name, last_name=last_name, education=education,
+        experience=experience, industry=industry)
+        profile.put()
+        self.redirect('/')
+
+class Display_Profile(webapp2.RequestHandler):
+    def get(self):
+        urlsafe_key = self.request.get('key')
+        key = ndb.Key(urlsafe=urlsafe_key)
+        profile=key.get()
+        logging.info(profile)
+        templateVars = {
+            'profile' : profile,
+        }
+        template = env.get_template('/templates/profile.html')
+        self.response.write(template.render(templateVars))
+    def post(self):
+        self.redirect("/create")
+
+
+
+class ResumeReview(webapp2.RequestHandler):
+    def get(self):
+        template = env.get_template("templates/resume_upload.html")
+        self.response.write(template.render())
+
 class ResumeUpload(webapp2.RequestHandler):
     def post(self):
         resume = self.request.get('resume')
@@ -118,7 +220,7 @@ class Login_Fail(webapp2.RequestHandler):
         template = env.get_template('/templates/login_fail.html')
         self.response.write(template.render(templateVar))
 
-class printAdvice(webapp2.RequestHandler):
+class ResumeAdvice(webapp2.RequestHandler):
     def get(self):
         dead_match = find_dead_words()
         action_match = find_action_words()
@@ -127,28 +229,26 @@ class printAdvice(webapp2.RequestHandler):
             'dead_match' : dead_match,
             'action_match' : action_match
         }
-        template = env.get_template('templates/resume_advice')
+        template = env.get_template('templates/resume_advice.html')
         self.response.write(template.render(templateVars))
+
 
 def parse_resume():
     current_user = users.get_current_user()
     current_profile = Profile.query().get()
     resume = current_profile.resume
 
-    content = ' '.join(resume)#.replace('\n','').replace('\r','').lower()
+    #content = ' '.join(resume)#.replace('\n','').replace('\r','').lower()
+    # words = {}
+    wordArray = resume.split(' ')
+    # print wordArray
+    # for word in wordArray:
+    #     if (word in words and word is not ''):
+    #         words[word] += 1
+    #     else:
+    #         words[word] = 1
 
-    print content
-    words = {}
-    wordArray = content.split(" ")
-
-    print wordArray
-    for word in wordArray:
-        if (word in words and word is not ''):
-            words[word] += 1
-        else:
-            words[word] = 1
-
-    return words
+    return wordArray
 
 def find_action_words():
     action_match = {}
@@ -177,8 +277,8 @@ app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/create', CreateProfile),
     ('/profile', Display_Profile),
-    ('/upload_resume', ResumeUpload),
+    ('/resume_review', ResumeReview),
+    ('/resume_advice', ResumeAdvice),
     ('/resume', ResumeHandler),
-    ('/advice', printAdvice),
     ('/fail', Login_Fail),
 ], debug=True)
